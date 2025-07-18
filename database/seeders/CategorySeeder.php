7a8475a43845652670a61ecf38238e04b1538d25
haikaljs <haikaljavascript@gmail.com>
2025-07-18 22:41:03 +0800

    DepartmentSeeder, CategorySeeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+                // Top level categories (depth 0)
+                [
+                    'name' => 'Electronics',
+                    'department_id' => 1, //assuming department_id 1 is for Electronics
+                    'parent_id' => null,
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+
+                ],
+                [
+                    'name' => 'Fashion',
+                    'department_id' => 2, // assuming department_id 2 is for Fashion
+                    'parent_id' => null,
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+
+                // Subcategories of electronics (depth 1)
+                [
+                    'name' => 'Computers',
+                    'department_id' => 1, 
+                    'parent_id' => 1, // parent is Electronics
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'name' => 'Smartphones',
+                    'department_id' => 1, 
+                    'parent_id' => 1, // parent is Electronics
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+
+                // Subcategories of Computers (depth 2)
+                 [
+                    'name' => 'Laptops',
+                    'department_id' => 1, 
+                    'parent_id' => 3, // parent is Computers
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                 [
+                    'name' => 'Desktops',
+                    'department_id' => 1, 
+                    'parent_id' => 3, // parent is Computers
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+
+                // Subcategories of smartphones (depth 2)
+                [
+                    'name' => 'Android',
+                    'department_id' => 1, 
+                    'parent_id' => 4, // parent is Smartphones
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+                [
+                    'name' => 'Iphone',
+                    'department_id' => 1, 
+                    'parent_id' => 4, // parent is Smartphones
+                    'active' => true,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ],
+
+            ];
+
+            DB::table('categories')->insert($categories);
+    }
+}
